7ededaadd4fbc4c331b9752e291092bef3203c83
Dayane Carolina Oliveira <102394029+dayanecol@users.noreply.github.com>
2023-01-23 17:34:04 -0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,10 +1,82 @@
-CREATE DATABASE songs;
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE playlist(
-	id serial NOT NULL,
-	favorite BOOLEAN,
-	title TEXT NOT NULL,
-	artist TEXT NOT NULL,
-	genre TEXT NOT NULL,
-	album TEXT NOT NULL
+-- Dumped from database version 12.13 (Ubuntu 12.13-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.13 (Ubuntu 12.13-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: playlist; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.playlist (
+    id integer NOT NULL,
+    favorite boolean,
+    title text NOT NULL,
+    artist text NOT NULL,
+    genre text NOT NULL,
+    album text NOT NULL
 );
+
+
+--
+-- Name: playlist_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.playlist_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: playlist_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.playlist_id_seq OWNED BY public.playlist.id;
+
+
+--
+-- Name: playlist id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.playlist ALTER COLUMN id SET DEFAULT nextval('public.playlist_id_seq'::regclass);
+
+
+--
+-- Data for Name: playlist; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.playlist VALUES (2, NULL, '3', 'NOVO', 'rock', 'novo');
+INSERT INTO public.playlist VALUES (3, NULL, '2', 'NOVO', 'rock', 'novo');
+INSERT INTO public.playlist VALUES (1, true, '4', 'NOVO', 'rock', 'novo');
+
+
+--
+-- Name: playlist_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.playlist_id_seq', 4, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
